fd5d669cf41c3eb8d581fdb9ec9afcb4946e5357
Ulises <ulies.torrella@gmail.com>
2016-06-27 09:21:24 -0300

    Cambios de formato en views

--- Solcito/views.py
+++ Solcito/views.py
@@ -10,48 +10,48 @@ def submitMatricula(request):
     context = RequestContext(request)
     if request.method=='POST':
         print "POST"
-        nombre=request.POST['nombre']
-        apellido=request.POST['apellido']
-        dni=request.POST['dni']
-        religion=request.POST['religion']
-        sexo=request.POST['sexo']
-        nacimiento=request.POST['nacimiento']
-        lugar_nacimiento = request.POST['lugar_nacimiento']
-        nacionalidad=request.POST['nacionalidad']
-        calle=request.POST['calle']
-        altura=request.POST['altura']
-        barrio=request.POST['barrio']
-        torre=request.POST['torre']
-        piso=request.POST['piso']
-        departamento=request.POST['departamento']
-        postal=request.POST['postal']
-        localidad=request.POST['localidad']
-        mail=request.POST['mail']
-        fijo=request.POST['fijo']
-        celular=request.POST['celular']
+        alumno_nombre=request.POST['nombre']
+        alumno_apellido=request.POST['apellido']
+        alumno_dni=request.POST['dni']
+        alumno_religion=request.POST['religion']
+        alumno_sexo=request.POST['sexo']
+        alumno_nacimiento=request.POST['nacimiento']
+        alumno_lugar_nacimiento = request.POST['lugar_nacimiento']
+        alumno_nacionalidad=request.POST['nacionalidad']
+        alumno_calle=request.POST['calle']
+        alumno_altura=request.POST['altura']
+        alumno_barrio=request.POST['barrio']
+        alumno_torre=request.POST['torre']
+        alumno_piso=request.POST['piso']
+        alumno_departamento=request.POST['departamento']
+        alumno_postal=request.POST['postal']
+        alumno_localidad=request.POST['localidad']
+        alumno_mail=request.POST['mail']
+        alumno_fijo=request.POST['fijo']
+        alumno_celular=request.POST['celular']
         alumno = Student()
-        alumno.name = nombre
-        alumno.lastName = apellido
-        alumno.dni = int(dni)
-        alumno.sex = sexo
-        alumno.religion = religion
-        alumno.birthDate = nacimiento
-        alumno.birthPlace = lugar_nacimiento
-        alumno.nacionality = nacionalidad
-        alumno.street = calle
-        alumno.numberStreet = int(altura)
-        alumno.neighborhood = barrio
-        alumno.tower = torre
-        print torre
-        if piso != "":
-            alumno.floorDepartment = int(piso)
-            print piso
-        alumno.department = departamento
-        alumno.PC = int(postal)
-        alumno.nacionality = nacionalidad
-        alumno.email = mail
-        alumno.landline = int(fijo)
-        if celular != "":
-            alumno.cellphone = int(celular)
+        alumno.name = alumno_nombre
+        alumno.lastName = alumno_apellido
+        alumno.dni = int(alumno_dni)
+        alumno.sex = alumno_sexo
+        alumno.religion = alumno_religion
+        alumno.birthDate = alumno_nacimiento
+        alumno.birthPlace = alumno_lugar_nacimiento
+        alumno.nacionality = alumno_nacionalidad
+        alumno.street = alumno_calle
+        alumno.numberStreet = int(alumno_altura)
+        alumno.neighborhood = alumno_barrio
+        alumno.tower = alumno_torre
+        print alumno_torre
+        if alumno_piso != "":
+            alumno.floorDepartment = int(alumno_piso)
+            print alumno_piso
+        alumno.department = alumno_departamento
+        alumno.PC = int(alumno_postal)
+        alumno.nacionality = alumno_nacionalidad
+        alumno.email = alumno_mail
+        alumno.landline = int(alumno_fijo)
+        if alumno_celular != "":
+            alumno.cellphone = int(alumno_celular)
         alumno.save()
         return render_to_response('matricular_success.html',{},context)

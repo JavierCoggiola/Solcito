828549d74fb616d4723d3d36b448e2a8214a1474
Ulises <ulies.torrella@gmail.com>
2016-10-04 11:18:42 -0300

    started with django admin

--- ITSGestion/settings.py
+++ ITSGestion/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -37,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Solcito',
-    'material',
 ]
 
 MIDDLEWARE_CLASSES = [
--- Solcito/admin.py
+++ Solcito/admin.py
@@ -2,26 +2,17 @@ from django.contrib import admin
 from Solcito.models import Student, RegistrationS, Tutor, Assistance, Discipline, Curso, Marks, Subject, RegistrationD, Teacher
 # Register your models here.
 
-class RegistrationAdmin(admin.ModelAdmin):
-    exclude = ()
-    search_fields = ('nameStudent',)
-    list_display = ('nameStudent','lastNameStudent','_get_foto')
 
-    def _get_foto(self, obj):
-         return obj.get_foto()
-    _get_foto.allow_tags = True
-
-class RegistrationAdminFoto(admin.ModelAdmin):
-    exclude = ()
-    list_display = ('photo',)
-
-    def _get_foto(self, obj):
-         return obj.get_foto()
-    _get_foto.allow_tags = True
 
 admin.site.register(Student)
 admin.site.register(Tutor)
-admin.site.register(RegistrationS)
+
+class RegistrationSAdmin(admin.ModelAdmin):
+
+    list_filter = ('activeDate', 'curso')
+
+
+admin.site.register(RegistrationS,RegistrationSAdmin)
 admin.site.register(Assistance)
 admin.site.register(Discipline)
 admin.site.register(Curso)

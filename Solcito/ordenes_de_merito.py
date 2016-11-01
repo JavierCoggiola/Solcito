36ea035b9b3f734193f55e469a34404d6b5ebad4
Ulises <ulises.torrella@gmail.com>
2016-11-01 14:41:38 -0300

    added results front end to orden de merito

--- Solcito/ordenes_de_merito.py
+++ Solcito/ordenes_de_merito.py
@@ -5,7 +5,11 @@ from Solcito.models import OrdenDeMerito, Curso
 def oredenes_de_merito(request):
     context = RequestContext(request)
     if request.user.groups.filter(name='Administrador').exists():
-        return render_to_response('ordenes_de_merito.html', {'ordenes': OrdenDeMerito.objects.all(),'cursos':Curso.objects.all()}, context)
+        anos = []
+        anos = list(set([curso.cycle for curso in Curso.objects.all()]))
+        return render_to_response('ordenes_de_merito.html', {'ordenes': OrdenDeMerito.objects.all(),
+                                                             'cursos':Curso.objects.all(),
+                                                             'anos': anos}, context)
     else:
         return redirect('/admin')
 
